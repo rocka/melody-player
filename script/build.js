bd8902218952cb81d6b295166c0a84c59882eab9
Rocka <i@rocka.me>
2018-04-27 21:46:25 +0800

    package: add build script

--- script/build.js
+++ script/build.js
@@ -2,9 +2,13 @@
 
 const webpack = require('webpack');
 
-const cfg = require('./webpack.conofig');
+if (process.argv.includes('--dev')) {
+    process.env.NODE_ENV = 'development';
+} else if (process.argv.includes('--prod')) {
+    process.env.NODE_ENV = 'production';
+}
 
-webpack(cfg, (err, stats) => {
+webpack(require('./webpack.conofig'), (err, stats) => {
     process.stdout.write(stats.toString({ colors: true }));
     if (err || stats.hasErrors()) {
         process.exitCode = 1;
