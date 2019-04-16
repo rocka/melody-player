2613645ff60870186ccb112ca76d06b1656771b7
Rocka <i@rocka.me>
2019-04-16 16:04:45 +0800

    build: build process for 'preload.css'

--- script/build.js
+++ script/build.js
@@ -1,6 +1,10 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+const less = require('less');
 const webpack = require('webpack');
+const CleanCSSPlugin = require('less-plugin-clean-css');
 
 if (process.argv.includes('--dev')) {
     process.env.NODE_ENV = 'development';
@@ -13,4 +17,15 @@ webpack(require('./webpack.conofig'), (err, stats) => {
     if (err || stats.hasErrors()) {
         process.exitCode = 1;
     }
+});
+
+const preloadCss = fs.readFileSync(path.join(__dirname, '../src/preload.less'), 'utf8');
+less.render(preloadCss, {
+    plugins: [new CleanCSSPlugin({ level: 2 })]
+}).then(r => {
+    fs.writeFileSync(path.join(__dirname, '../dist/preload.css'), r.css);
+}).catch(e => {
+    console.error('Error when processing "preload.less":');
+    console.error(e);
+    process.exitCode = 1;
 });
